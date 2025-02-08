2dab2b8e0281c2946a6b3ef97c2836dc69fb0e81
Iganza-roy <iganzaroy55@gmail.com>
2025-02-08 22:16:50 +0300

    Add Contact component with form submission and contact details; enhance layout and styling

--- src/pages/Support.jsx
+++ src/pages/Support.jsx
@@ -1,7 +1,156 @@
 import React from 'react';
+import { FiMail } from 'react-icons/fi';
+import { IoLogoWhatsapp } from 'react-icons/io';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { FaLocationDot } from 'react-icons/fa6';
+import { useState } from 'react';
+import { toast } from 'sonner';
 
-const Support = () => {
-  return <div></div>;
+const Contact = () => {
+  const [result, setResult] = React.useState('');
+
+  const handleNavigate = (webLink) => {
+    window.open(webLink, '_blank');
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult('Sending....');
+    const formData = new FormData(event.target);
+
+    formData.append('access_key', 'f622c96c-57d3-47e6-a296-d95ceefae30f');
+
+    const response = await fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      toast.success('From submitted successfully');
+      setResult('Form Submitted Successfully');
+      event.target.reset();
+    } else {
+      console.log('Error', data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <div className='flex flex-col gap-10'>
+      <div className='flex justify-center items-center'>
+        <h1 className='relative text-light font-bold text-3xl lg:text-4xl text-shadow-sm before:absolute before:rounded-md before:-bottom-[0.1px] before:left-0 before:w-full before:h-[13px] before:bg-blue before:-z-10  hover:before:bg-pink'>
+          Contact Us
+        </h1>
+      </div>
+      <div className='flex flex-col md:flex-row justify-center items-center gap-12 lg:gap-[200px]'>
+        <div className='flex flex-col justify-center items-center gap-6'>
+          <div className='flex flex-row gap-4 justify-center items-center'>
+            <FaLocationDot className='text-slate-600 text-5xl hover:text-black ' />
+            <div className='flex flex-col gap-1'>
+              <p className='text-slate-600 text-lg cursor-pointer hover:text-black '>
+                Location
+              </p>
+              <p
+                className='hover:text-black text-slate-600 cursor-pointer'
+                onClick={() =>
+                  handleNavigate('https://maps.app.goo.gl/mtzLV2hufrP5TyBy8')
+                }
+              >
+                P.O. Box 001-10000, KPR
+                <br />
+                Tom Mboya Strt, Nairobi
+              </p>
+            </div>
+          </div>
+          <div className='flex flex-row gap-4 justify-start items-center w-full'>
+            <IoLogoWhatsapp className='text-slate-600 text-5xl hover:text-black ' />
+            <div className='flex flex-col gap-1'>
+              <p className='text-slate-600 text-lg cursor-pointer hover:text-blac'>
+                Whatsapp
+              </p>
+              <p
+                className='hover:text-black text-slate-600 cursor-pointer'
+                onClick={() => handleNavigate('https://wa.me/+2547000000000')}
+              >
+                +2547000000000
+              </p>
+            </div>
+          </div>
+          <div className='flex flex-row gap-4 justify-start items-center w-full'>
+            <FaPhoneAlt className='text-slate-600 text-5xl hover:text-black ' />
+            <div className='flex flex-col gap-1'>
+              <p className='text-slate-600 text-lg cursor-pointer hover:text-black '>
+                Telephone
+              </p>
+              <p className='hover:text-black text-slate-600 cursor-pointer'>
+                +2547000000000
+              </p>
+            </div>
+          </div>
+          <div className='flex flex-row gap-4 justify-start items-center w-full'>
+            <FiMail className='text-slate-600 text-5xl hover:text-black ' />
+            <div className='flex flex-col gap-1'>
+              <p className='text-slate-600 text-lg cursor-pointer hover:text-black'>
+                Email
+              </p>
+              <p
+                className='hover:text-black text-slate-600 cursor-pointer'
+                onClick={() => handleNavigate('https://mail.google.com')}
+              >
+                info@kopero.ac.ke
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='flex flex-col justify-center items-center gap-4 bg-gray1 rounded-md py-[40px] px-[30px] lg:px-[60px] '>
+          <p className='text-lg'>Let us hear from you</p>
+          <div>
+            <form
+              action=''
+              className='flex flex-col gap-6 justify-center items-center'
+              onSubmit={onSubmit}
+            >
+              <div className='flex flex-row gap-4'>
+                <input
+                  type='text'
+                  placeholder='First Name'
+                  required
+                  className='rounded-md w-[150px] h-10 text-black p-2 bg-white '
+                />
+                <input
+                  type='text'
+                  required
+                  placeholder='Last Name'
+                  className='rounded-md w-[150px] h-10 text-black p-2 bg-white'
+                />
+              </div>
+              <input
+                type='email'
+                placeholder='Email Address'
+                required
+                className='rounded-md w-[320px] h-10 text-black p-2 bg-white'
+              />
+              <textarea
+                type='text'
+                placeholder='Enter Your Message'
+                required
+                className='rounded-sm w-[320px] h-[120px] text-black p-2 bg-white box-border'
+              />
+              <button
+                type='submit'
+                className='bg-slate-900 rounded-md w-[100px] h-[40px] text-white transition:transform duration-300 hover:bg-slate-800 hover:border-3 hover:-translate-y-1'
+              >
+                Submit
+              </button>
+            </form>
+            <span>{result}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Support;
+export default Contact;
